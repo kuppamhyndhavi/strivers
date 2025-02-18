923cb73317f59b484b3128b40a734ae783910d09
Kuppam Hyndhavi <153989010+kuppamhyndhavi@users.noreply.github.com>
2025-02-18 10:16:15 +0530

    Add files via upload

--- Patterns/N-Triangles.cpp
+++ Patterns/N-Triangles.cpp
@@ -1,11 +1,10 @@
-void nForest(int n) {
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cout<<"* ";
-			if(j<n-1)cout<<"";
+void nTriangle(int n) {
+	// Write your code here
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout<<j ;
+			if(j<i)cout<<" ";
 		}
 		cout<<endl;
 	}
 }
-
-
